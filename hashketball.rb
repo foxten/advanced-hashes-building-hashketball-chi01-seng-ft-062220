5d8af6815e521567dbd5264dd85541ce98e447a7
Tenaysia Fox <tenaysiafox@gmail.com>
2020-05-30 02:02:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -60,6 +60,16 @@ def game_hash
       :team_name =>"Charlotte Hornets",
       :colors =>["turquoise", "purple"],
       :players =>[
+        { :player_name => "Jeff Adrien",
+          :number => 4,
+          :shoe => 18,
+          :points => 10,
+          :rebounds => 1,
+          :assists => 1,
+          :steals => 2,
+          :blocks => 7,
+          :slam_dunks => 
+        },          
         { :player_name => "",
           :number => ,
           :shoe => ,
@@ -69,9 +79,8 @@ def game_hash
           :steals => ,
           :blocks => ,
           :slam_dunks => 
-        },          
-          
-        }
+        },     
+    
         ],
     }
   }
